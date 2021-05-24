5fc1e9c355ff6acb36573681e7f7482075aa094c
ogawa-yuri <nomiyuri713@gmail.com>
2021-05-24 20:58:42 +0900

    編集画面作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,10 +13,20 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find_by(id: params[:id])
   end
 
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.all.order(updated_at: :desc)
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "投稿を編集しました！"
+    else
+      render :edit
+    end
   end
 
   def destroy
